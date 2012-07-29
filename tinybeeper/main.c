e9773e4a70671fca286ddc7e98a8c27be7b44981
Michael Buesch <m@bues.ch>
2012-07-29 12:51:45 +0200

    Update email address
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tinybeeper/main.c
+++ tinybeeper/main.c
@@ -1,7 +1,7 @@
 /*
  *  Morse code beeper
  *
- *  Copyright (C) 2010 Michael Buesch <mb@bu3sch.de>
+ *  Copyright (C) 2010 Michael Buesch <m@bues.ch>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
